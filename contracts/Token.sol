a4e31a54b9ac41651579e1a8b8f5235ad63cb136
Ringaile Valiaugaite <ringaile@gmail.com>
2019-06-30 15:14:40 +0200

    implemented token

---
+++ contracts/Token.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract HonestToken is ERC20, ERC20Detailed  {
+ 
+	string public name = "HonestToken";
+	string public symbol = "HT";
+	uint8 public decimals = 8;
+	uint public INITIAL_SUPPLY = 200000;
+
+	constructor() public {
+  		_mint(msg.sender, INITIAL_SUPPLY);
+	}
+}
